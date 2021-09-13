b9ae5c0eee415334ed8c07f1581623517ef15351
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-09-13 19:44:59 -0300

    Tweak add function.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import './style.css';
-
 let btn = document.getElementById('btn');
+
+function deleteBook(event) {
+  event.currentTarget;
+}
 
 function add() {
   let autor = document.getElementById('autor');
   let title = document.getElementById('title');
   let books = document.getElementById('books');
-  
-  books.appendChild(autor);
+
+  autorText = document.createElement('p');
+  titleText = document.createElement('p');
+  deleteBtn = document.createElement('button');
+
+  deleteBtn.innerHTML = 'Delete';
+
+  autorText.innerHTML = autor.value;
+  titleText.innerHTML = title.value;
+
+  deleteBtn.addEventListener('click', deleteBook);
+
+
+
+  books.appendChild(autorText);
+  books.appendChild(titleText);
+  books.appendChild(deleteBtn);
 // document.body.appendChild(books);
 }
 
