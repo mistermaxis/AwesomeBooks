b2961b1841270dce2559583dd84c0cd2404ab678
marysol <marilirulita@gmail.com>
2021-09-17 12:59:51 -0700

    Add classes and statict methods

--- src/index.js
+++ src/index.js
@@ -1,82 +1,87 @@
+function Book(title, autor, id) {
+  this.bookTitle = title;
+  this.bookAuthor = autor;
+  this.id = id;
+}
+
 const btn = document.getElementById('btn');
 let ids = 0;
 
 let bookList = [];
 
-function displayBooks() {
-  const books = document.getElementById('books');
-  books.innerHTML = '';
+class BooksList{
 
-  bookList.forEach((book) => {
-    const bookDiv = document.createElement('div');
-    const autorText = document.createElement('p');
-    const titleText = document.createElement('p');
-    const deleteBtn = document.createElement('button');
+  static displayBooks() {
+    const books = document.getElementById('books');
+    books.innerHTML = '';
+  
+    bookList.forEach((book) => {
+      const bookDiv = document.createElement('div');
+      const autorText = document.createElement('p');
+      const titleText = document.createElement('p');
+      const deleteBtn = document.createElement('button');
+  
+      /* eslint-disable */
+      deleteBtn.addEventListener('click', BooksList.deleteBook);    
+      /* eslint-enable */
+  
+      deleteBtn.innerHTML = 'Delete';
+      bookDiv.id = book.id;
+  
+      autorText.innerHTML = book.bookAuthor;
+      titleText.innerHTML = book.bookTitle;
+  
+      bookDiv.appendChild(autorText);
+      bookDiv.appendChild(titleText);
+      bookDiv.appendChild(deleteBtn);
+  
+      books.appendChild(bookDiv);
+    });
+  }
 
-    /* eslint-disable */
-    deleteBtn.addEventListener('click', deleteBook);    
-    /* eslint-enable */
+  static loadBooks() {
+    const local = window.localStorage.getItem('awesomebooks');
+  
+    if (local != null) {
+      bookList = JSON.parse(local);
+    }
+  }
 
-    deleteBtn.innerHTML = 'Delete';
-    bookDiv.id = book.id;
+  static update() {
+    BooksList.loadBooks();
+    BooksList.displayBooks();
+  }
+  
+  static saveBooks() {
+    window.localStorage.setItem('awesomebooks', JSON.stringify(bookList));
+  }
+  
+  static deleteBook(event) {
+    const targetDiv = event.currentTarget.parentElement;
+    const divId = targetDiv.id;
+  
+    const filter = bookList.filter((book) => book.id !== divId);
+    bookList = filter;
+  
+    BooksList.saveBooks();
+    BooksList.displayBooks();
+  }
 
-    autorText.innerHTML = book.bookAuthor;
-    titleText.innerHTML = book.bookTitle;
-
-    bookDiv.appendChild(autorText);
-    bookDiv.appendChild(titleText);
-    bookDiv.appendChild(deleteBtn);
-
-    books.appendChild(bookDiv);
-  });
-}
-
-function loadBooks() {
-  const local = window.localStorage.getItem('awesomebooks');
-
-  if (local != null) {
-    bookList = JSON.parse(local);
+  static add() {
+    const autor = document.getElementById('autor');
+    const title = document.getElementById('title');
+  
+    const book = new Book(title.value, autor.value, `id${ids}`)
+  
+    ids += 1;
+  
+    bookList.push(book);
+  
+    BooksList.saveBooks();
+    BooksList.displayBooks();
   }
 }
 
-function update() {
-  loadBooks();
-  displayBooks();
-}
+window.addEventListener('load', BooksList.update);
 
-function saveBooks() {
-  window.localStorage.setItem('awesomebooks', JSON.stringify(bookList));
-}
-
-function deleteBook(event) {
-  const targetDiv = event.currentTarget.parentElement;
-  const divId = targetDiv.id;
-
-  const filter = bookList.filter((book) => book.id !== divId);
-  bookList = filter;
-
-  saveBooks();
-  displayBooks();
-}
-
-window.addEventListener('load', update);
-
-function add() {
-  const autor = document.getElementById('autor');
-  const title = document.getElementById('title');
-
-  const book = {
-    bookAuthor: autor.value,
-    bookTitle: title.value,
-    id: `id${ids}`,
-  };
-
-  ids += 1;
-
-  bookList.push(book);
-
-  saveBooks();
-  displayBooks();
-}
-
-btn.addEventListener('click', add);
+btn.addEventListener('click', BooksList.add);
